bbddf1d8b86468fa317967a01cae943530383d8a
thien0709 <thien070904@gmail.com>
2023-03-16 19:11:01 +0700

    update heart

--- access/main.js
+++ access/main.js
@@ -1,2 +1,13 @@
 const btn = document.querySelectorAll(".login button");
-const heart
+const heart = document.querySelectorAll(".pic .heart");
+const count = document.querySelectorAll(".heart h6");
+let arr = [10];
+for(let i = 0;i<10;i++){
+    arr[i]=0;
+}
+for(let i=0;i<heart.length;i++){
+    heart[i].onclick = function(){
+        arr[i]++;
+        count[i].innerHTML = arr[i];
+    }
+}
